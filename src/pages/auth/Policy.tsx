ef498b047ff223fd15f3b19426cb8146df568f27
eddiekodebaze <eddie.ling@kodebaze.com>
2024-07-02 14:21:35 +0800

    Add policy and terms page

---
+++ src/pages/auth/Policy.tsx
@@ -0,0 +1,29 @@
+const Policy = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '90vh',
+        marginLeft: -200,
+        flexDirection: 'column',
+        gap: 12,
+        fontFamily: 'Titillium Web',
+      }}
+    >
+      <div
+        style={{
+          fontWeight: 600,
+          fontSize: 24,
+          color: '#152023',
+          marginBottom: 30,
+        }}
+      >
+        Privacy Policy
+      </div>
+    </div>
+  );
+};
+
+export default Policy;
