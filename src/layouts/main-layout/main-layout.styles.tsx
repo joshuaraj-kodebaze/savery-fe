0a81dd263fa3374481bb015713929b121f5a95ab
Joshua Raj <joshuapravin21@gmail.com>
2024-07-03 16:12:34 +0800

    adjusted main layout height

--- src/layouts/main-layout/main-layout.styles.tsx
+++ src/layouts/main-layout/main-layout.styles.tsx
@@ -2,13 +2,10 @@
 import { styled } from '@mui/material';
 
 export const LayoutContainer = styled('div')(({ theme }) => ({
-  height: 'calc(100vh - 110px)',
-
+  height: 'calc(100vh - 50px)',
   marginTop: 50,
   marginLeft: 208,
-
   padding: '24px 24px 24px 40px',
-
   [theme.breakpoints.down('sm')]: {
     marginLeft: 0,
   },
