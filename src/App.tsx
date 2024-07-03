08959e2d8da62dc794e0cd4a8ae71e9c475c7728
Joshua Raj <joshuapravin21@gmail.com>
2024-07-03 19:57:16 +0800

    latest updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // import './App.css'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 // Import layouts
 import MainLayout from 'layouts/main-layout/main-layout';
@@ -11,6 +11,7 @@ import Saml from 'pages/saml/saml';
 import Policy from 'pages/policy/policy';
 import Terms from 'pages/terms/terms';
 import General from 'pages/general/general';
+import ErrorPage from 'pages/error-page/error-page';
 
 // Import utils
 import { ROUTES } from 'utils/constants';
@@ -18,18 +19,20 @@ import { ROUTES } from 'utils/constants';
 function App() {
   return (
     <Routes>
+      <Route path="404" element={<ErrorPage />} />
+      <Route path="*" element={<Navigate to="/404" replace />} />
       <Route element={<MainLayout />}>
         <Route index path="/" element={<Projects />} />
-        <Route index path={ROUTES.user.LOGIN} element={<Login />} />
-        <Route index path={ROUTES.user.SAML} element={<Saml />} />
-        <Route index path={ROUTES.user.POLICY} element={<Policy />} />
-        <Route index path={ROUTES.user.TERMS} element={<Terms />} />
         <Route
           index
           path={ROUTES.settings.GENERAL}
           element={<General />}
         />
       </Route>
+      <Route index path={ROUTES.user.LOGIN} element={<Login />} />
+      <Route index path={ROUTES.user.SAML} element={<Saml />} />
+      <Route index path={ROUTES.user.POLICY} element={<Policy />} />
+      <Route index path={ROUTES.user.TERMS} element={<Terms />} />
     </Routes>
   );
 }
