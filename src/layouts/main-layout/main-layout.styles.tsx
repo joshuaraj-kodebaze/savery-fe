f7e7beec3e73e62e7b82b89d529c2bfd55ab2ee0
Joshua Raj <joshuapravin21@gmail.com>
2024-07-03 19:29:48 +0800

    latest updates

--- src/layouts/main-layout/main-layout.styles.tsx
+++ src/layouts/main-layout/main-layout.styles.tsx
@@ -8,7 +8,6 @@ export const LayoutContainer = styled('div')(({ theme }) => ({
   padding: '24px 24px 24px 40px',
   [theme.breakpoints.down('md')]: {
     marginLeft: 0,
-    padding: '24px 24px 24px 25px', //eddie - may remove, reducing left padding for mobile
   },
   [theme.breakpoints.down('sm')]: {
     padding: '16px',
