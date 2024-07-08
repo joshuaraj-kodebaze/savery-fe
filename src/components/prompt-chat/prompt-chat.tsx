8bd0c6df97c71b01770731329028fbdf0d8967a5
eddiekodebaze <eddie.ling@kodebaze.com>
2024-07-08 17:08:57 +0800

    Fix username position

--- src/components/prompt-chat/prompt-chat.tsx
+++ src/components/prompt-chat/prompt-chat.tsx
@@ -1,5 +1,5 @@
 // Import libraries
-import { Box, Typography, type InputProps } from '@mui/material';
+import { Box } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRobot } from '@fortawesome/pro-regular-svg-icons';
 
@@ -26,33 +26,36 @@ const PromptChat = (props: PromptChatProps) => {
       <Box
         sx={{
           display: 'flex',
-          flexDirection: 'column',
           gap: '8px',
           marginBottom: '10px',
-          alignItems: position === 'left' ? 'flex-start' : 'flex-end',
+          justifyContent:
+            position === 'left' ? 'flex-start' : 'flex-end',
         }}
       >
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            gap: '8px',
-          }}
-        >
-          <FontAwesomeIcon
-            icon={faRobot}
-            style={{
-              fontSize: 12,
-              backgroundColor: backgroundColor,
-              padding: 4,
-              borderRadius: '100px',
+        <div>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              gap: '8px',
+              marginBottom: '8px',
             }}
-          />
-          <Username>{username}</Username>
-        </Box>
-        <TextContainer style={{ backgroundColor: backgroundColor }}>
-          {text}
-        </TextContainer>
+          >
+            <FontAwesomeIcon
+              icon={faRobot}
+              style={{
+                fontSize: 12,
+                backgroundColor: backgroundColor,
+                padding: 4,
+                borderRadius: '100px',
+              }}
+            />
+            <Username>{username}</Username>
+          </Box>
+          <TextContainer style={{ backgroundColor: backgroundColor }}>
+            {text}
+          </TextContainer>
+        </div>
       </Box>
     </div>
   );
